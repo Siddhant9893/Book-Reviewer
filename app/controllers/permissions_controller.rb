dfce15c21a641533ca16bdf66317c0db11c77ad4
Siddhant9893 <rajpootsiddhantsingh@gmail.com>
2025-06-04 11:53:04 +0530

    Added notice and alert and code formating

--- app/controllers/permissions_controller.rb
+++ app/controllers/permissions_controller.rb
@@ -2,8 +2,6 @@ class PermissionsController < ApplicationController
 before_action :authenticate_user!
   def create
     @book=Book.find(params[:book_id])
-
-    
     permission=current_user.permissions.find_or_initialize_by(book:@book)
     permission.status ="pending"
     if permission.save 
@@ -18,7 +16,7 @@ before_action :authenticate_user!
     if permission.book.user==current_user
       permission.update(status: 'approved')
       redirect_to book_path(permission.book), notice: "access approved"
-     else
+    else
       redirect_to books_path
     end
   end
